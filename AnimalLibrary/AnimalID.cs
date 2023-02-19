49a76f7ff33a7126a19afb76edf1611e90ccc338
gchazov <cheyzov@outlook.com>
2023-02-19 18:31:56 +0500

    comment for interface

--- AnimalLibrary/AnimalID.cs
+++ AnimalLibrary/AnimalID.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace AnimalLibrary
 {
+    //класс-id животного в зоопарке
     public class AnimalId
     {
         public int number;
--- AnimalLibrary/IInit.cs
+++ AnimalLibrary/IInit.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace AnimalLibrary
 {
+    //интерфейс, реализуемый в программе
     public interface IInit
     {
         void Init();
